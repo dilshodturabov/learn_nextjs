9f536f0c90ce56060722c12787258e888e001ca5
dilshod turabov <85446738+dilshodturabov@users.noreply.github.com>
2021-07-19 09:04:12 +0500

    continnued useEffect hooks

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,49 @@
 import React, { useEffect, useState } from "react";
+import "../styles/Home.module.css";
 import { Fragment } from "react";
+
+let renderCount = 1;
 export default function Home() {
+  // useEffect hook
   useEffect(() => {
     document.title = "This site is fake!!";
   });
-  let isabookShopOpen = false;
-  let WillIgetNewBook = new Promise((resolve, reject) => {
-    if (isabookShopOpen) {
-      let book = {
-        author: "John Uik",
-        name: "Clear History",
-      };
-      resolve(book);
-    } else {
-      reject(console.log("Errror"));
-      console.log(reject);
-    }
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
   });
-  console.log(WillIgetNewBook);
 
+  useEffect(() => {
+    window.addEventListener("mousemove", (event) => {
+      setPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    });
+  }, []);
+  // end useEffect hook
+
+  // other hook
+  const [value, setValue] = useState("default");
+  useEffect(() => {
+    renderCount++;
+  });
   return (
     <Fragment>
-      <ul>{}</ul>
+      {/* useState hook */}
+      <div className="forUse container text-center ">
+        {JSON.stringify(position, null, 2)}
+      </div>
+      {/* end useEffect hook */}
+
+      {/* other hook */}
+      <h1>Amount of rendering {renderCount}</h1>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      {/* end other hook */}
     </Fragment>
   );
 }
